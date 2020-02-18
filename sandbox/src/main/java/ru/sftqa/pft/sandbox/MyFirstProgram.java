4a7e1e7a2ae3aeb0e3cf4c219e888bd577caae92
Dmitrii77 <dim.filev@gmail.com>
2020-02-17 21:40:30 -0800

    Created constructors: Square and Rectangle

--- sandbox/src/main/java/ru/sftqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sftqa/pft/sandbox/MyFirstProgram.java
@@ -6,13 +6,10 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Dmitrii");
 
-		Square s = new Square();
-		s.l = 5;
+		Square s = new Square(5);
 		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-		Rectangle r = new Rectangle();
-		r.a = 4;
-		r.b = 6;
+		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь треугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
   }
 
--- sandbox/src/main/java/ru/sftqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/sftqa/pft/sandbox/Square.java
@@ -3,4 +3,8 @@ package ru.sftqa.pft.sandbox;
 public class Square {
 
   public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
 }
